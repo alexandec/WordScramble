6959aef5a9b217a634efa0023cda7ee8b501a9ca
Chris Alexander <chris.alexander@adhocteam.us>
2024-06-24 15:17:46 -0600

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Christopher Michael Alexander on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
